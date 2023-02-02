f95c563fea8481b358f7a311e880f324ddfa758b
nbietz <nbietz@gmail.com>
2023-02-02 00:23:48 -0800

    Show NFT image, use magik-link, show wallet addr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,67 @@
 import type { AppProps } from "next/app";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { 
+  ChainId, 
+  WalletConnectConnectorType,
+  InjectedConnectorType,
+  ThirdwebProvider } from "@thirdweb-dev/react";
+import { MagicConnector } from "@thirdweb-dev/react/evm/connectors/magic";
 import "../styles/globals.css";
 import Head from "next/head";
-import ThirdwebGuideFooter from "../components/GitHubLink";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
+console.log("activeChainId ", activeChainId);
 
+// This is the meta-transaction relayer that provides gasless transactions
+const relayerUrlProp = process.env.NEXT_PUBLIC_GASLESS_OPENZEPPELIN_RELAYER_URL as string;
+console.log("relayerUrlProp ", relayerUrlProp);
+
+
+// Set up wallet connectors you want to use for your dApp.
+const magicLinkConnector = new MagicConnector({
+  options: {
+    apiKey: process.env.NEXT_PUBLIC_MAGIC_LINK_API_KEY as string,
+    rpcUrls: {
+      [activeChainId]: process.env.NEXT_PUBLIC_CHAIN_RPC as string,
+    }
+  }
+});
+const metamaskConnector: InjectedConnectorType = {
+  name: "metamask",
+  options: {
+    shimDisconnect: true
+  }
+};
+const walletConnector: WalletConnectConnectorType = {
+  name: "walletConnect",
+  options: { 
+    chainId: activeChainId.valueOf()
+  }
+};
+const  connectors = [
+  metamaskConnector,
+  walletConnector,
+  magicLinkConnector
+];
+
+// Main app function
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
-      <Head>
+    <ThirdwebProvider 
+      sdkOptions={{
+        gasless: {
+          openzeppelin: {
+            relayerUrl: relayerUrlProp,
+          },
+        },
+      }} 
+      chainRpc={{
+        [activeChainId]: process.env.NEXT_PUBLIC_CHAIN_RPC as string,
+      }}
+      walletConnectors={connectors}
+      desiredChainId={activeChainId}
+    >
+    <Head>
         <title>thirdweb Edition Drop Minting Customizable Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
@@ -23,7 +74,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
-      <ThirdwebGuideFooter />
     </ThirdwebProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import {
   useActiveClaimConditionForWallet,
   useAddress,
+  useDisconnect,
   useClaimConditions,
   useClaimerProofs,
   useClaimIneligibilityReasons,
   useContract,
   useContractMetadata,
+  useNFT,
   useTotalCirculatingSupply,
   Web3Button,
-} from "@thirdweb-dev/react";
+  useWalletConnect,
+  useMetamask } from "@thirdweb-dev/react";
+import { useMagic } from "@thirdweb-dev/react/evm/connectors/magic";
 import { BigNumber, utils } from "ethers";
 import type { NextPage } from "next";
 import { useMemo, useState } from "react";
@@ -16,17 +20,23 @@ import styles from "../styles/Theme.module.css";
 import { parseIneligibility } from "../utils/parseIneligibility";
 
 // Put Your Edition Drop Contract address from the dashboard here
-const myEditionDropContractAddress =
-  "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
+const myEditionDropContractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_NFT_EDITION_DROP as string;
+console.log("myEditionDropContractAddress ", myEditionDropContractAddress);
 
-// Put your token ID here
+// Token ID to mint
 const tokenId = 0;
 
 const Home: NextPage = () => {
   const address = useAddress();
-  const [quantity, setQuantity] = useState(1);
+  const connectWithWalletConnect = useWalletConnect(); // Hook to connect with WalletConnect.
+  const connectWithMetamask = useMetamask(); // Hook to connect with MetaMask.
+  const connectWithMagic = useMagic(); // Hook to connect with Magic Link.
+  const disconnectWallet = useDisconnect(); // Hook to disconnect from the connected wallet.
+  const [email, setEmail] = useState<string>(""); // State to hold the email address the user entered.
+  const [quantity, setQuantity] = useState(1); // Quantity of tokens to mint when Mint button is pressed
   const { contract: editionDrop } = useContract(myEditionDropContractAddress);
   const { data: contractMetadata } = useContractMetadata(editionDrop);
+  const { data: nft, isLoading: loadingNFT } = useNFT(editionDrop, tokenId);
 
   const claimConditions = useClaimConditions(editionDrop);
   const activeClaimCondition = useActiveClaimConditionForWallet(
@@ -219,111 +229,178 @@ const Home: NextPage = () => {
           <p>Loading...</p>
         ) : (
           <>
-            <div className={styles.infoSide}>
-              {/* Title of your NFT Collection */}
-              <h1>{contractMetadata?.name}</h1>
-              {/* Description of your NFT Collection */}
-              <p className={styles.description}>
-                {contractMetadata?.description}
-              </p>
-            </div>
-
-            <div className={styles.imageSide}>
-              {/* Image Preview of NFTs */}
-              <img
-                className={styles.image}
-                src={contractMetadata?.image}
-                alt={`${contractMetadata?.name} preview image`}
-              />
-
-              {/* Amount claimed so far */}
-              <div className={styles.mintCompletionArea}>
-                <div className={styles.mintAreaLeft}>
-                  <p>Total Minted</p>
-                </div>
-                <div className={styles.mintAreaRight}>
-                  {claimedSupply ? (
-                    <p>
-                      <b>{numberClaimed}</b>
-                      {" / "}
-                      {numberTotal || "∞"}
-                    </p>
-                  ) : (
-                    // Show loading state if we're still loading the supply
-                    <p>Loading...</p>
-                  )}
-                </div>
+              <div className={styles.infoSide}>
+                {/* Image Preview of NFTs */}
+                <img
+                  className={styles.image}
+                  src={contractMetadata?.image}
+                  alt={`${contractMetadata?.name} preview image`}
+                />
+                {/* Title of your NFT Collection */}
+                <h1>{contractMetadata?.name}</h1>
+                {/* Description of your NFT Collection */}
+                <p className={styles.description}>
+                  {contractMetadata?.description}
+                </p>
               </div>
 
-              {claimConditions.data?.length === 0 ||
-              claimConditions.data?.every(
-                (cc) => cc.maxClaimableSupply === "0"
-              ) ? (
-                <div>
-                  <h2>
-                    This drop is not ready to be minted yet. (No claim condition
-                    set)
-                  </h2>
-                </div>
-              ) : (
-                <>
-                  <p>Quantity</p>
-                  <div className={styles.quantityContainer}>
-                    <button
-                      className={`${styles.quantityControlButton}`}
-                      onClick={() => setQuantity(quantity - 1)}
-                      disabled={quantity <= 1}
-                    >
-                      -
-                    </button>
+              <div className={styles.imageSide}>
+                {/* Image Preview of NFTs */}
+              <img
+                className={styles.image}
+                src={nft?.metadata?.image!}
+                alt={`${nft?.metadata?.name} preview image`}
+              />
 
-                    <h4>{quantity}</h4>
-
-                    <button
-                      className={`${styles.quantityControlButton}`}
-                      onClick={() => setQuantity(quantity + 1)}
-                      disabled={quantity >= maxClaimable}
-                    >
-                      +
-                    </button>
+                {/* Amount claimed so far */}
+                <div className={styles.mintCompletionArea}>
+                  <div className={styles.mintAreaLeft}>
+                    <p>Total Minted</p>
                   </div>
-
-                  <div className={styles.mintContainer}>
-                    {isSoldOut ? (
-                      <div>
-                        <h2>Sold Out</h2>
-                      </div>
+                  <div className={styles.mintAreaRight}>
+                    {claimedSupply ? (
+                      <p>
+                        <b>{numberClaimed}</b>
+                        {" / "}
+                        {numberTotal || "∞"}
+                      </p>
                     ) : (
-                      <Web3Button
-                        contractAddress={editionDrop?.getAddress() || ""}
-                        action={(cntr) => cntr.erc1155.claim(tokenId, quantity)}
-                        isDisabled={!canClaim || buttonLoading}
-                        onError={(err) => {
-                          console.error(err);
-                          alert("Error claiming NFTs");
-                        }}
-                        onSuccess={() => {
-                          setQuantity(1);
-                          alert("Successfully claimed NFTs");
-                        }}
-                      >
-                        {buttonLoading ? "Loading..." : buttonText}
-                      </Web3Button>
+                      // Show loading state if we're still loading the supply
+                      <p>Loading...</p>
                     )}
                   </div>
-                </>
-              )}
-            </div>
+                </div>
+
+                {claimConditions.data?.length === 0 ||
+                claimConditions.data?.every(
+                  (cc) => cc.maxClaimableSupply === "0"
+                ) ? (
+                  <div>
+                    <h2>
+                      This drop is not ready to be minted yet. (No claim condition
+                      set)
+                    </h2>
+                  </div>
+                ) : (
+                  <>
+                    <div className={styles.mintContainer}>
+                      {isSoldOut ? (
+                        <div>
+                          <h2>Sold Out</h2>
+                        </div>
+                      ) : (
+                        <>
+                          <>
+                            {address ? (
+                              <div className={styles.loginContainer}>
+                                <Web3Button
+                                  contractAddress={editionDrop?.getAddress() || ""}
+                                  action={(cntr) => cntr.erc1155.claim(tokenId, quantity)}
+                                  isDisabled={!canClaim || buttonLoading}
+                                  onError={(err) => {
+                                    console.error(err);
+                                    alert("Error claiming NFTs");
+                                  }}
+                                  onSuccess={() => {
+                                    setQuantity(1);
+                                    alert("Successfully claimed NFTs");
+                                  }}
+                                >
+                                  {buttonLoading ? "Loading..." : buttonText}
+                                </Web3Button>
+
+                                <h2 style={{ fontSize: "1.3rem" }}>You&apos;re Connected! 👋</h2>{" "}
+                                <p>{address}</p>
+                                <a className={styles.mainButton} onClick={() => disconnectWallet()}>
+                                  Disconnect Wallet
+                                </a>
+                              </div>
+                            ) : (
+                              <>
+                                <div
+                                  style={{
+                                    width: 600,
+                                    maxWidth: "90vw",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    flexDirection: "row",
+                                    gap: 16,
+                                  }}
+                                >
+                                  <button 
+                                    className={styles.mainButton} 
+                                    onClick={connectWithWalletConnect}>
+                                      Use WalletConnect
+                                  </button>
+                                </div>
+                                <div
+                                  style={{
+                                    height: 15,
+                                  }}
+                                >
+                                </div>
+                                <div
+                                  style={{
+                                    width: 600,
+                                    maxWidth: "90vw",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    flexDirection: "row",
+                                    gap: 16,
+                                  }}
+                                >
+                                  <button 
+                                    className={styles.mainButton} 
+                                    onClick={connectWithMetamask}>
+                                      Use MetaMask
+                                  </button>
+                                </div>
+
+                                <h2 style={{ fontSize: "1.3rem" }}>- Or -</h2>
+                                <h1 style={{ fontSize: "1.1rem" }}>Use your email if you don't have a crypto wallet...</h1>
+                                
+                                <div
+                                  style={{
+                                    width: 500,
+                                    maxWidth: "90vw",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    flexDirection: "row",
+                                    gap: 16,
+                                  }}
+                                >
+                                  <input
+                                    type="email"
+                                    placeholder="Log In With Email"
+                                    className={styles.textInput}
+                                    style={{ width: "90%", marginBottom: 0 }}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                  />
+
+                                  <a
+                                    className={styles.mainButton}
+                                    onClick={() => {
+                                      connectWithMagic({ email });
+                                    }}
+                                  >
+                                    Login
+                                  </a>
+                                </div>
+                              </>
+                            )}
+                          </>
+                        </>
+                      )}
+                    </div>
+                  </>
+                )}
+              </div>
           </>
         )}
       </div>
-      {/* Powered by thirdweb */}{" "}
-      <img
-        src="/logo.png"
-        alt="thirdweb Logo"
-        width={135}
-        className={styles.buttonGapTop}
-      />
     </div>
   );
 };
